5a2e2b3bd55de560a3e4d1c7ad2a95ea694ed554
Qizhen-lin <54734015+Qizhen-lin@users.noreply.github.com>
2021-07-21 17:05:06 +0800

    新增資料

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// 網頁的主程式寫在這裡
+// 綁定 .ad-toggle-btn 的點擊事件
+$(".ad-toggle-btn").click(function () {
+    console.log("[.ad-toggle-btn被點擊了]");
+    // 讓.side-box 被切換class="active"
+    $(".side-box").toggleClass("active");
+
+});
+
+// 綁定 #removeSideBoxBtn 的點擊事件
+$("#removeSideBoxBtn").click(function () {
+    console.log("[#removeSideBoxBtn被點擊了]");
+    $(".side-box").fadeOut(500);
+
+});
+
+$("#goBackBtn").click(function () {
+    console.log("滑到最上方");
+    const duration = 300;
+    $("html, body").stop().animate({
+        scrollTop:0
+    }, duration);
+});
